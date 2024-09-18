41bb27f409bdc75f8bf5bb89cf5f96cd65590d9c
Diego Fernando <diegofernandofonseca@hotmail.com>
2024-09-17 20:08:13 -0500

    My First Component

---
+++ practice_01/src/components/MyFirstComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return (
+    <>
+    <div>
+        My FirstComponent
+    </div>
+    <h1>Estoy Aprendiendo Componentes en React</h1>
+    <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados Hooks</li>
+        <li>Props</li>
+        
+    </ul>
+    </>
+  )
+}
+
+export default MyFirstComponent
